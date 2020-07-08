dcff2dadc1139384b25b1d479883bfd71e40b679
Java <alcidelnikob1982@gmail.com>
2020-07-08 18:05:50 +0700

    basics

---
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -0,0 +1,32 @@
+package ru.progwards.java1.lessons.basics;
+
+public class AccuracyDoubleFloat {
+
+    public static double volumeBallDouble(double radius){
+        Double pi=3.14;
+        Double V = 4.0/3.0*pi*(radius*radius*radius);
+        return V;
+    }
+
+
+    public static float volumeBallFloat(float radius){
+        float pi= (float) 3.14;
+//        return (float) volumeBallDouble(6371.2);
+        float V = (float) (4.0/3.0*pi*(radius*radius*radius));
+        return V;
+}
+
+    public static double calculateAccuracy(double radius){
+        float temp =volumeBallFloat((float) 6371.2);
+        String str = Float.toString(temp);
+        double str2 = Double.parseDouble(str);
+        double raz=volumeBallDouble(6371.2)-str2;
+        return raz;
+    }
+    public static void main (String[]args){
+
+        System.out.println(volumeBallDouble( 6371.2));
+        System.out.println(volumeBallFloat((float) 6371.2));
+        System.out.println(calculateAccuracy(6371.2));
+    }
+}
---
+++ src/ru/progwards/java1/lessons/basics/Astronomy.java
@@ -0,0 +1,40 @@
+package ru.progwards.java1.lessons.basics;
+
+public class Astronomy<sphereSquare> {
+
+    public static Double sphereSquare(Double r){
+        Double pi=3.14;
+        Double S = 4*pi*r*r;
+        return S;
+    }
+
+    public static  Double earthSquare(){
+
+        return sphereSquare(6371.2);
+    }
+    public static  Double mercurySquare(){
+
+       return sphereSquare(2439.7);
+    }
+
+    public static  Double jupiterSquare(){
+        return sphereSquare(71492.0);
+    }
+
+    public static Double earthVsMercury(){
+        Double raz =sphereSquare(6371.2)/sphereSquare(2439.7);
+        return raz;
+    }
+    public static Double earthVsJupiter(){
+        Double raz1 =sphereSquare(6371.2)/sphereSquare(71492.0);
+        return raz1;
+    }
+    public static void main (String[]args){
+        System.out.println(earthSquare());
+        System.out.println(mercurySquare());
+        System.out.println(jupiterSquare());
+        System.out.println(earthVsMercury());
+        System.out.println(earthVsJupiter());
+
+    }
+}
