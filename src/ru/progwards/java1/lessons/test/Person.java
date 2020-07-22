ee10164acac6b01740d6e39fd74b7166a1858818
Java <alcidelnikob1982@gmail.com>
2020-07-22 21:52:23 +0700

    5lesson

---
+++ src/ru/progwards/java1/lessons/test/Person.java
@@ -0,0 +1,32 @@
+
+
+
+/*
+Создайте класс Person, содержащий
+приватную переменную String name;
+приватную переменную int age;
+приватную переменную String country;
+конструктор по умолчанию, устанавливающий country = "RU";
+конструктор с параметрами String name, int age, вызывающий конструктор по умолчанию,
+и устанавливающий значения переменным name и age из соответствующих параметров;
+публичные функции String getName(), int getAge(), String getCountry(), возвращающие значения
+соответствующих переменных*/
+/*
+class Person {
+    private String name;
+    private int age;
+    private String country;
+
+    public Person() {
+        country = "RU";
+    }
+    public Person(String name, int age) {
+        this();
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {return name;}
+    public int getAge() {return age;}
+    public String getCountry() {return country;}
+}
+*/
