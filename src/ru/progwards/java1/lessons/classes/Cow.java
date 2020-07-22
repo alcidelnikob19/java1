e13094332d5f6303c7dcc9cbe0aca2c8d0bf357d
Java <alcidelnikob1982@gmail.com>
2020-07-22 22:25:16 +0700

    5lesson

--- src/ru/progwards/java1/lessons/classes/Cow.java
+++ src/ru/progwards/java1/lessons/classes/Cow.java
@@ -4,6 +4,18 @@ public class Cow extends Animal{
     private AnimalKind animalKind = AnimalKind.COW;
     private FoodKind foodKind = FoodKind.HAY;
 
+    private double weight = 1d; // вес животного
+    private double foodCoeff = 0.05; // коэффициент веса еды к весу тела животного
+
+    Cow() {
+    }
+
+    public Cow(double weight) { // не нужный объект, когда не задан тип животного
+        this.weight = weight;
+    }
+    public double getFoodCoeff() {
+        return foodCoeff;
+    }
     public AnimalKind getKind() {
         return animalKind;
     }
