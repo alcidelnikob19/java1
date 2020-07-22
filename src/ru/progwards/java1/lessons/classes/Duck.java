d260fb7334e02bb9616aaa693ec76e3270e0ea48
Java <alcidelnikob1982@gmail.com>
2020-07-22 22:31:58 +0700

    5lesson

--- src/ru/progwards/java1/lessons/classes/Duck.java
+++ src/ru/progwards/java1/lessons/classes/Duck.java
@@ -3,6 +3,16 @@ package ru.progwards.java1.lessons.classes;
 public class Duck extends Animal{
     private AnimalKind animalKind = AnimalKind.DUCK;
     private FoodKind foodKind = FoodKind.CORN;
+
+    private double weight = 1d; // вес животного
+    private double foodCoeff = 0.04; // коэффициент веса еды к весу тела животного
+
+    Duck() {
+    }
+
+    public Duck(double weight) { // не нужный объект, когда не задан тип животного
+        this.weight = weight;
+    }
     public AnimalKind getKind() {
         return animalKind;
     }
--- src/ru/progwards/java1/lessons/classes/Hamster.java
+++ src/ru/progwards/java1/lessons/classes/Hamster.java
@@ -3,6 +3,15 @@ package ru.progwards.java1.lessons.classes;
 public class Hamster extends Animal{
     private AnimalKind animalKind = AnimalKind.HAMSTER;
     private FoodKind foodKind = FoodKind.CORN;
+    private double weight = 1d; // вес животного
+    private double foodCoeff = 0.03; // коэффициент веса еды к весу тела животного
+
+    Hamster() {
+    }
+
+    public  Hamster(double weight) { // не нужный объект, когда не задан тип животного
+        this.weight = weight;
+    }
     public AnimalKind getKind() {
         return animalKind;
     }
