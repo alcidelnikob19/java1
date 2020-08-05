ef107b242d3f125d69b97aa17de5510c0506d5d3
Java <alcidelnikob1982@gmail.com>
2020-08-05 22:38:58 +0700

    6lesson

---
+++ src/ru/progwards/java1/lessons/test/Main63.java
@@ -0,0 +1,19 @@
+//package ru.progwards.java1.lessons.test;
+//
+//import java.util.Arrays;
+//
+//public class Main63 {
+//    public static int arrayMax(int[] w) {
+//
+//        Arrays.sort(w);
+//        return (w.length) > 0 ? w[(w.length - 1)] : 0;
+//    }
+//
+//        public  static void main(String[] args) {
+//
+//            int[] ar = {1,2};
+//            System.out.println(arrayMax(ar));
+//
+//        }
+//    }
+
