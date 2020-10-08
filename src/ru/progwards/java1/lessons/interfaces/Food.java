d81f37b4c9c2ce9de0550647dc242b28f7220dc7
Java <alcidelnikob1982@gmail.com>
2020-10-08 19:25:05 +0700

    8lesson2

--- src/ru/progwards/java1/lessons/interfaces/Food.java
+++ src/ru/progwards/java1/lessons/interfaces/Food.java
@@ -1,24 +1,34 @@
 package ru.progwards.java1.lessons.interfaces;
 
-public class Food implements CompareWeight{
+public class Food implements CompareWeight {
     private int weight;
-    public int getWeight(int weight){
-        return  weight;
+
+    Food(int weight) {
+        this.weight = weight;
     }
+
     public double getWeight() {
 
         return weight;
     }
+
     @Override
     public CompareResult compareWeight(CompareWeight smthHasWeigt) {
         return null;
     }
+
     public CompareResult CompareResult(CompareWeight smthHasWeigt) {
-        Food food = (Food)smthHasWeigt;
+        Food food = (Food) smthHasWeigt;
 
         if (this.weight < food.weight) return CompareResult.LESS;
         if (this.weight == food.weight) return CompareResult.EQUAL;
         return CompareResult.GREATER;
+
+    }
+
+    public static void main(String[] args) {
+        Food food1 = new Food(40);
+        System.out.println(food1.CompareResult(food1));
 
     }
 }
