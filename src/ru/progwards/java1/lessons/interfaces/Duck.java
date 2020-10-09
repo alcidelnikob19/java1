dc57ce58500105626c6f4a2909e8f54f69815121
Java <alcidelnikob1982@gmail.com>
2020-10-09 22:46:17 +0700

    8lesson2

--- src/ru/progwards/java1/lessons/interfaces/Duck.java
+++ src/ru/progwards/java1/lessons/interfaces/Duck.java
@@ -3,7 +3,7 @@ package ru.progwards.java1.lessons.interfaces;
 public class Duck extends Animal {
 
     public Duck(double weight) { // не нужный объект, когда не задан тип животного
-        super(AnimalKind.DUCK, FoodKind.HAY, weight,0.04);
+        super(AnimalKind.DUCK, FoodKind.CORN, weight,0.04);
 
     }
 
