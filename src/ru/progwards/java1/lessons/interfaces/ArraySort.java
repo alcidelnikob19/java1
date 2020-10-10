286f507c118bddf3abfa1ffa533b572cf50486e8
Java <alcidelnikob1982@gmail.com>
2020-10-10 12:51:30 +0700

    8lesson2

--- src/ru/progwards/java1/lessons/interfaces/ArraySort.java
+++ src/ru/progwards/java1/lessons/interfaces/ArraySort.java
@@ -21,18 +21,13 @@ package ru.progwards.java1.lessons.interfaces;
 //}
         public class ArraySort {
 
-            // public static void sort (Comparable [] a) {
-            public static void sort(Comparable<Animal>[] a) {
-                Comparable tmpValue;
+            public static void sort(CompareWeight[] a) {
+                CompareWeight tmpValue;
                 for (int i = a.length - 1; i > 0; i--) {
-                    // найдем в остатках максимальный
+
                     for (int j = 0; j < i; j++) {
-                        // if (a [j] .compareTo (a [i])> 0) {
-                        if (a[j].compareTo((Animal) a[i]) > 0) {
-                            tmpValue = a[i];
-                            a[i] = a[j];
-                            a[j] = tmpValue;
-                        }
+
+
                     }
                 }
             }
