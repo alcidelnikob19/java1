07b90864157d322190f0979f1d8f48975d58bba4
Java <alcidelnikob1982@gmail.com>
2020-10-10 11:58:06 +0700

    8lesson2

--- src/ru/progwards/java1/lessons/interfaces/ArraySort.java
+++ src/ru/progwards/java1/lessons/interfaces/ArraySort.java
@@ -5,11 +5,18 @@ public  class  ArraySort  {
 
     // public static void sort (Comparable [] a) {++
     public static void sort(CompareWeight[] a){
+        int indexMin;
+        for (int index = 0; index < a.length ; index++){
+            indexMin = index;
+            for (int indexToScan = index; indexToScan < a.length; indexToScan++) {
+//                if (a[indexMin] > a[indexToScan]) {
+//                    indexMin = indexToScan;
+//                }
+            }
+            CompareWeight temp = a[index];
+            a[index] = a[indexMin];
+            a[indexMin]=temp;
 
-       for ( int i = a . length -  1 ; i >=  0 ; i -- ){
-            for (int j = 0; j < i; j++) {
-
-           }
-       }
+        }
     }
 }
