e05c8390a0fd2192aae91f38be10e1e1515029c0
Java <alcidelnikob1982@gmail.com>
2020-10-12 23:57:24 +0700

    8lesson2

--- src/ru/progwards/java1/lessons/interfaces/ArraySort.java
+++ src/ru/progwards/java1/lessons/interfaces/ArraySort.java
@@ -1,35 +1,61 @@
 package ru.progwards.java1.lessons.interfaces;
 
 
-//public  class  ArraySort {
+//public static void sort(int[] a){
 //
-//    public static void sort(CompareWeight[] a) {
+//
+//        //проходим по массиву
+//        //Найти минимальный элемент
+//        //меняем местами 1 и минимальный элемент
+//
+//        //повторяем пока не пройхоме по всему массиву
 //        int indexMin;
 //        for (int index = 0; index < a.length ; index++){
-//            indexMin = index;
-//            for (int indexToScan = index; indexToScan < a.length; indexToScan++) {
-////                if (a[indexMin] > a[indexToScan]) {
-////                    indexMin = indexToScan;
-////                }
-//            }
-//            CompareWeight temp = a[index];
-//            a[index] = a[indexMin];
-//            a[indexMin]=temp;
+//        indexMin = index;
+//        for (int indexToScan = index; indexToScan < a.length; indexToScan++) {
+//        if (a[indexMin] > a[indexToScan]) {
+//        indexMin = indexToScan;
+//        }
+//        }
+//        int temp = a[index];
+//        a[index] = a[indexMin];
+//        a[indexMin]=temp;
 //
 //        }
-//    }
-//}
+//        System.out.println(Arrays.toString(a));
+//        }
+//public static void main(String[] args) {
+//        int[] unsortedArray = {2,1,45,7};
+//
+//        System.out.println(Arrays.toString(unsortedArray));
+//
+//        sort(unsortedArray);
+//        }
         public class ArraySort {
 
             public static void sort(CompareWeight[] a) {
-                CompareWeight tmpValue;
-                for (int i = a.length - 1; i > 0; i--) {
-
-                    for (int j = 0; j < i; j++) {
-
-
+                int indexMin;
+                for (int index = 0; index < a.length ; index++){
+                    indexMin = index;
+                    for (int indexToScan = index; indexToScan < a.length; indexToScan++) {
+//                        public CompareWeight.CompareResult compareWeight(CompareWeight smthHasWeigt) {
+//                            Food food = (Food) smthHasWeigt;
+//
+//                            if (this.weight < food.weight) return CompareWeight.CompareResult.LESS;
+//                            if (this.weight == food.weight) return CompareWeight.CompareResult.EQUAL;
+//                            return CompareWeight.CompareResult.GREATER;
+//
+//                        }
+                        if (a[indexMin] > a[indexToScan]) {
+                            indexMin = indexToScan;
+                        }
                     }
+                    CompareWeight temp = a[index];
+                    a[index] = a[indexMin];
+                    a[indexMin]=temp;
+
                 }
+
             }
         }
 
