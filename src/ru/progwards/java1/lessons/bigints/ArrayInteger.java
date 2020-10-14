308235dfef01d79f0d8a2b66f9f5ddacd77f3094
Java <alcidelnikob1982@gmail.com>
2020-10-14 19:11:25 +0700

    9lesson1

---
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -0,0 +1,19 @@
+package ru.progwards.java1.lessons.bigints;
+import java.math.*;
+public class ArrayInteger {
+    public static BigInteger ArrayInteger(int n){
+        return null;
+        }
+    void fromInt(BigInteger value){
+
+    }
+
+    BigInteger toInt() {
+        return null;
+    }
+
+    boolean add(ArrayInteger num){
+
+        return false;
+    }
+}
