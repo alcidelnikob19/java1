2b6781d202f02e528b1495b8f2e3fe0073e9cc41
Java <alcidelnikob1982@gmail.com>
2020-11-07 18:29:20 +0700

    9lesson1

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -1,11 +1,7 @@
 package ru.progwards.java1.lessons.bigints;
 
-import ru.progwards.java1.lessons.arrays.DIntArray;
-
 public abstract class AbsInteger {
-    static AbsInteger add(AbsInteger num1, AbsInteger num2) {
-        return int i = (int);
+
     }
 
-}
 
