fd41afd74aa508e6ea6bfeddc74e26712eba0f00
Java <alcidelnikob1982@gmail.com>
2020-11-15 07:46:43 +0300

    9lesson1

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -1,19 +1,30 @@
 package ru.progwards.java1.lessons.bigints;
 import java.math.*;
+import java.util.Arrays;
 public class ArrayInteger {
-    public static BigInteger ArrayInteger(int n){
-        return null;
-        }
-    void fromInt(BigInteger value){
+
+//    Реализовать класс ArrayInteger - целого числа произвольной длины на массиве byte[] digits; Каждый элемент массива
+//    digits[i] может хранить только цифру, то есть число от 0 до 9. Например, число 159 должно занять 3 ячейки массива
+//    digits[0] = 9; digits[1] = 5; digits[2] = 1;
+//
+//    Реализовать методы:
+//
+//            3.1 ArrayInteger(int n) - инициализирует класс, с максимальной точностью n цифр (размер массива)
+//
+//3.2 void fromInt(BigInteger value) - установить свое значение, взяв его из value (уложить BigInteger во внутренний массив)
+//
+//3.3 BigInteger toInt() - привести свое значение к BigInteger (преобразовать из массива в BigInteger)
+//
+//3.4 boolean add(ArrayInteger num) - сложить 2 числа, не используя BigInteger, а  используя массив digits, результат
+// поместить в экземпляр ArrayInteger, у которого был вызван метод. При переполнении вернуть false, при этом само число
+// сбросить в 0
+//
+
+    byte[] digits;
+
+    public ArrayInteger(int n){
+        digits=new byte[n];
 
     }
 
-    BigInteger toInt() {
-        return null;
-    }
-
-    boolean add(ArrayInteger num){
-
-        return false;
-    }
 }
