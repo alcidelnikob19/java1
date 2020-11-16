a142d69badd99db5b075443e8e86bbf4ec62e9bb
Java <alcidelnikob1982@gmail.com>
2020-11-16 15:49:38 +0300

    9lesson1

---
+++ src/ru/progwards/java1/lessons/sources/Solution.java
@@ -0,0 +1,89 @@
+package ru.progwards.java1.lessons.sources;
+public class Solution<c> {
+    public static void main(String[] args) {
+//        int a = -7;
+//        if (a>0)
+//        System.out.println(a);
+//+++++++++
+//        int a = 77;
+//        int b;
+//        if (a > 10) {
+//            b = 1;
+//        } else {
+//            b = -1;
+//        }
+
+
+//        +++++++++++++++++
+//        int a = 7;
+//        int b;
+//        b = (a>10)?1:-1;
+//        System.out.println("b = "+b);
+
+//        +++++++
+//        int a = 4;
+//        if (a < 0) {
+//
+//            System.out.println("a < 0");}
+//          else
+//            System.out.println("a > 0");
+//        }
+//++++++++++++++++
+//    int a = 8;
+//        if (a == 8) {
+//        System.out.println("a < 10 AND a < 100");}
+//}
+//++++++++++++++++++
+//    double c= 100 / 3d;
+//        System.out.println(100 / 3d);
+//}
+//************
+
+//        int a = 101;
+//        int b;
+//        if (a > 0) {
+//                if (a < 100) {
+//        b = 1;
+//        } else {
+//        b = 0;
+//        }
+//        } else {
+//        b = -1;
+//        }
+//        b = a>0?a<100?1:0:-1;
+//        System.out.println("b = "+b);
+//        }
+//        ++++++
+//        int a = 0;
+//        int c;
+//        c = a > 0 ? 1 : -1;
+//        System.out.println("c = " + c);
+//    }
+
+//****
+//    String str = "A";
+//        switch str{
+//        case "A";
+//        System.out.println("A");
+//    }
+//        if (str.equals("A")) {
+//        System.out.println("A");
+//    } else if (str.equals("B")) {
+//        System.out.println("B");
+//    } else {
+//        System.out.println("C");
+//    }
+        String str = "C";
+        switch (str) {
+            case "A":
+                System.out.println("A");
+                break;
+            case "B":
+                System.out.println("B");
+                break;
+            case "C":
+                System.out.println("C");
+                break;
+        }
+    }
+}
